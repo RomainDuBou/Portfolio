a73265cdf8e03cc4c6caabde7c379a67744db6ec
RomainRDB <rduboullay@gmail.com>
2024-11-21 14:50:18 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
       <ProjectsSection />
       <TapeSection />
       <TestimonialsSection />
+      <AboutSection/>
     </div>
   );
 }
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,5 +1,18 @@
+import SectionHeader from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg"
+
 export const AboutSection = () => {
-  return 
-  <div>
-  </div>;
-};
+  return <div className="pb-96">
+    <SectionHeader eyebrow="A Propos de moi" title="Un petit pas dans mon monde" description="Apprenez en plus à propos de qui je suis, ce que je fais et ce qui m'inspire"/>
+    <div>
+      <Card>
+        <div>
+          <StarIcon />
+          <h3>Mes Livres</h3>
+          <p>Explorez mes livres favoris</p>
+        </div>
+      </Card>
+    </div>
+  </div>
+}
