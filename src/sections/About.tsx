653e1a206167ea7e0742b8415789c823668b649f
RomainRDB <rduboullay@gmail.com>
2024-11-22 11:45:51 +0100

    commit

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -54,44 +54,44 @@ const hobbies = [
   {
     title: "Musique",
     emoji: "🎺",
-    left: '50%',
-    top: '5%',
+    left: "50%",
+    top: "5%",
   },
   {
     title: "Jeux Vidéos",
     emoji: "🎮",
-    left: '35%',
-    top: '40%',
+    left: "35%",
+    top: "40%",
   },
   {
     title: "Randonnée",
     emoji: "🥾",
-    left: '10%',
-    top: '35%',
+    left: "10%",
+    top: "35%",
   },
   {
     title: "Sport",
     emoji: "🏋️‍♂️",
-    left: '70%',
-    top: '45%',
+    left: "70%",
+    top: "45%",
   },
   {
     title: "Films",
     emoji: "🎬",
-    left: '5%',
-    top: '65%',
+    left: "5%",
+    top: "65%",
   },
   {
     title: "Voitures",
     emoji: "🏎️",
-    left: '45%',
-    top: '70%',
+    left: "45%",
+    top: "70%",
   },
 ];
 
 export const AboutSection = () => {
   return (
-    <div className="pb-20">
+    <div className="pb-20 lg:py-28">
       <div className="container">
         <SectionHeader
           eyebrow="A Propos de moi"
@@ -99,54 +99,73 @@ export const AboutSection = () => {
           description="Apprenez en plus à propos de qui je suis, ce que je fais et ce qui m'inspire"
         />
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[350px]">
-            <CardHeader
-              title="Mes Livres"
-              description="Découvrez les livres qui m'ont le plus marqué"
-            />
-            <div className="justify-center items-center gap-4">
-              <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
-                <span className="font-semibold">Ma liste de lecture</span>
-                <ArrowUpRightIcon className="size-4" />
-              </button>
-            </div>
-            <div className="w-40 mx-auto mt-8">
-              <Image src={BookImage} alt="Book cover" />
-            </div>
-          </Card>
-          <Card className="h-[320px] p-0">
-            <CardHeader
-              title="Ma Boîte à Outils"
-              description="Explorez les technologies et les outils utilisés pour développer
-                mes compétences" className="px-6 pt-6"
-            />
-            <ToolboxItems items={toolboxItems} className="mt-2" />
-            <ToolboxItems items={toolboxItems} className="mt-6" 
-             itemsWrapperClassName="-translate-x-1/2"/>
-          </Card>
-          <Card className="h-[320px] p-0 flex flex-col">
-          <CardHeader
-              title="Au delà du code"
-              description="Explorez mes intérets et mes passions au delà du monde digital" className="px-6 pt-6"
-            />
-            <div className="relative flex-1">
-              {hobbies.map((hobby) => (
-                <div key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute" style={{ 
-                  left: hobby.left,
-                  top: hobby.top,
-                 }}>
-                  <span className="font-medium text-gray-950">{hobby.title}</span>
-                  <span>{hobby.emoji}</span>
-                </div>
-              ))}
-            </div>
-          </Card>
-          <Card className="h-[320px] p-0 relative">
-            <Image src={mapImage} alt="map" className="h-full w-full object-cover"/>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
-            <Image src={emoji} alt="smile emoji" className="size-20" />
-            </div>
-          </Card>
+          <div className="grid grid-cols-1 gap-8  md:grid-cols-5">
+            <Card className="h-[320px] md:col-span-2">
+              <CardHeader
+                title="Mes Livres"
+                description="Découvrez les livres qui m'ont le plus marqué"
+              />
+              <div className="justify-center items-center gap-4">
+                <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl md:text-sm -mt-5 ml-5">
+                  <span className="font-semibold">Ma liste de lecture</span>
+                  <ArrowUpRightIcon className="size-4" />
+                </button>
+              </div>
+              <div className="w-40 mx-auto mt-5">
+                <Image src={BookImage} alt="Book cover" />
+              </div>
+            </Card>
+            <Card className="h-[320px] md:col-span-3">
+              <CardHeader
+                title="Ma Boîte à Outils"
+                description="Explorez les technologies et les outils utilisés pour développer
+                mes compétences"
+                className=""
+              />
+              <ToolboxItems items={toolboxItems} className="" />
+              <ToolboxItems
+                items={toolboxItems}
+                className="mt-6"
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3">
+              <CardHeader
+                title="Au delà du code"
+                description="Explorez mes intérets et mes passions au delà du monde digital"
+                className="px-6 pt-6"
+              />
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className="font-medium text-gray-950">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0 relative md:col-span-2">
+              <Image
+                src={mapImage}
+                alt="map"
+                className="h-full w-full object-cover"
+              />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <Image src={emoji} alt="smile emoji" className="size-20" />
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -53,7 +53,7 @@ export const TestimonialsSection = () => {
                 {testimonials.map((testimonial) => (
                   <Card
                     key={testimonial.name}
-                    className="max-w-xs md:max-w-md md:p-8 hover:-rotate-3 transition duration-300"
+                    className="max-w-xs md:max-w-md p-6 md:p-8 hover:-rotate-3 transition duration-300"
                   >
                     <div className="flex gap-4 items-center">
                       <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
